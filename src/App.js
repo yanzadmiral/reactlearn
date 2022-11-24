ac7e4fda7ce5b0e6fabc2d3a2a2dd6cc04e38aae
Yayan Cahyani <yayancloud@gmail.com>
2022-11-24 11:48:24 +0700

    dinamis route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import './App.css';
+/**
+ * third party
+ */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/**
+ * internal source
+ */
+import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Genres from "./pages/Genres";
+import Admin from "./pages/Admin";
+
+/**
+ * style
+ */
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello word</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">go react movie project</h1>
+          <hr className="mt-3" />
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <Menu />
+          </div>
+          <div className="col-10">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/movies' element={<Movies />}/>
+              <Route path='/genres' element={<Genres />}/>
+              <Route path='/admin' element={<Admin />}/>
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
