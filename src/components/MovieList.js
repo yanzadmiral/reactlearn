1203420d83b75fdce14e0236dac1aeec0adb7a9f
Yayan Cahyani <yayancloud@gmail.com>
2022-11-24 14:28:08 +0700

    usestate dan use efect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,92 +1,35 @@
-import React from 'react'
+import React, { useState,useEffect} from 'react'
 
 const MovieList = () => {
+
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    setMovies([
+      { id: 1, title: "asdasd", runtime: 145 },
+      { id: 2, title: "12", runtime: 145 },
+      { id: 3, title: "asdasadds", runtime: 145 },
+    ]);
+  }, [])
+  
+
   return (
     <div className="row">
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+      {movies.map((movie, index) => ( 
+        <div className="col-sm-4 mb-2" key={index}>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              {/* <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a> */}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6 mb-2">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
